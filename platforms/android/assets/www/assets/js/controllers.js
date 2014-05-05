a1f5ff334a34023b0a79f8c30901b3f6ef3436b3
hatboysam <hatboysam@gmail.com>
2014-05-05 00:37:22 -0400

    Google Cal integration working, for now

--- platforms/android/assets/www/assets/js/controllers.js
+++ platforms/android/assets/www/assets/js/controllers.js
@@ -23,11 +23,24 @@ Weekday.prototype.hasTasks = function() {
   return (this.tasks.length > 0);
 }
 
+Weekday.prototype.clearTasks = function() {
+  this.tasks = [];
+}
+
 /**
  * Task object
  */
 function Task(description) {
   this.description = description;
+}
+
+function dateFromString(dateString) {
+  var splitArr = dateString.split("-");
+  var year = parseInt(splitArr[0]);
+  var month = parseInt(splitArr[1]) - 1;
+  var day = parseInt(splitArr[2]);
+
+  return new Date(year, month, day);
 }
 
 /**
@@ -58,14 +71,112 @@ weeklyApp.service('weekdayModel', ['$rootScope', function($rootScope) {
 weeklyApp.controller('DayCtrl', ['$scope', 'weekdayModel', function($scope, weekdayModel) {
   $scope.days = weekdayModel.days;
 
+  /** Log In button text **/
+  $scope.loginMsg = "Log In";
+
+  /** Google auth token **/
+  $scope.token = "";
+
+  /** Base calendar URL **/
+  $scope.baseURL = "https://www.googleapis.com/calendar/v3/";
+
   /**
    * Sign in with Google+
    */
-  $scope.signIn = function() {
-    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-      $scope.token = token;
-      console.log(token);
-      console.log(chrome.runtime.lastError);
+  $scope.logIn = function() {
+    gapi.auth.authorize({}, function(response) {
+      console.log(response);
+
+      if (response.access_token) {
+        console.log('Checking for calendars...')
+        $scope.checkCalendarsExist();
+      }
+    });
+  };
+
+  $scope.checkCalendarsExist = function() {
+    // COMPLETE CALENDAR
+    chrome.storage.local.get('completeId', function(items) {
+      if (items.completeId) {
+        // Calendar exists for complete
+        console.log(items.completeId);
+        $scope.completeId = items.completeId;
+      } else {
+        // Create calendar for complete
+        console.log('NEED TO CREATE COMPLETE');
+        $scope.createCalendar('Weekly Complete', function(calObj) {
+          if (calObj.id) {
+            chrome.storage.local.set({ completeId: calObj.id });
+          }
+        });
+      }
+    });
+
+    // INCOMPLETE CALENDAR
+    chrome.storage.local.get('incompleteId', function(items) {
+      if (items.incompleteId) {
+        // Calendar does not exist for complete
+        console.log(items.incompleteId)
+        $scope.incompleteId = items.incompleteId;
+      } else {
+        // Create calendar for incomplete
+        console.log('NEED TO CREATE INCOMPLETE');
+        $scope.createCalendar('Weekly Incomplete', function(calObj) {
+          if (calObj.id) {
+            chrome.storage.local.set({ incompleteId: calObj.id });
+          }
+        });
+      }
     });
   }
+
+  /**
+   * Create a calendar with a given name
+   */
+  $scope.createCalendar = function(name, callback) {
+    console.log('Creating Calendar...');
+    gapi.client.request({
+      path: '/calendar/v3/calendars',
+      method: 'POST',
+      body: { summary: name },
+      callback: callback
+    });
+  }
+
+  $scope.refresh = function() {
+    $scope.loadEvents($scope.incompleteId, function(response) {
+      console.log(response);
+      if (response.items) {
+        // Clear tasks
+        weekdayModel.days.forEach(function(day) {
+          day.clearTasks();
+        });
+
+        // Add back
+        response.items.forEach(function(item) {
+          var startDate = dateFromString(item.start.date);
+          var startDay = weekdayModel.days[startDate.getDay()];
+          startDay.addTask(new Task(item.summary));
+        });
+
+        // Add to scope
+        $scope.days = weekdayModel.days;
+        $scope.$digest();
+      }
+    });
+  };
+
+  /**
+   * Load all events from a calendar name
+   */
+  $scope.loadEvents = function(id, callback) {
+    console.log('Loading Events...');
+    gapi.client.request({ 
+      path: '/calendar/v3/calendars/' + id + '/events', 
+      callback: callback
+    });
+  }
+
 }]);
+
+
--- platforms/android/assets/www/assets/js/onpage.js
+++ platforms/android/assets/www/assets/js/onpage.js
@@ -1,11 +1,40 @@
+function gapiIsLoaded() {
+  console.log('GAPI is Loaded');
+}
+
+function serialize(obj) {
+   var str = [];
+   for(var p in obj){
+       if (obj.hasOwnProperty(p)) {
+           str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+       }
+   }
+   return str.join("&");
+}
+
 $(document).ready(function() {
 
-  // Show and hide tasks
-  $('.day').on({
+  // Side Panel menu
+  var jPM = $.jPanelMenu({
+    menu: '#menu',
+    trigger: '.menu-trigger',
+    excludedPanelContent: '#title',
+    animated: true
+  });
+  jPM.on();
+
+  // Clicking a side panel menu item
+  $('.menu-item').on({
     click: function() {
+      // Close the menu
+      jPM.close();
+    }
+  });
+
+  // Show and hide tasks
+  $(document).on('click', '.day', function() {
       $(this).siblings('.task').slideToggle();
       $(this).find('.badge').fadeToggle();
-    }
   });
 
 });
